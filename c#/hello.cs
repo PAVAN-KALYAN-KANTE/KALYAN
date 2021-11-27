26893aacbcc88520a91aff4d697cbd84786091a6
kalyan14-cloud <61815993+kalyan14-cloud@users.noreply.github.com>
2021-11-27 21:40:23 +0530

    Delete hello.cs

--- c#/hello.cs
+++
@@ -1,9 +0,0 @@
-using System;
-namespace hello{
-    class hello{
-        public static void Main(string[] args){
-            Console.WriteLine("HELLO WORLD");
-            Console.ReadLine();
-        }
-    }
-}
